edf3eb745b3ac4d758463fb7b74a56b1fec371c8
Asad Hussain <asad.h1998@yahoo.com>
2022-08-24 11:01:33 +0500

    updated details of enums

--- enumeration/enumeration.py
+++ enumeration/enumeration.py
@@ -36,3 +36,34 @@ print(Color['RED'], type(Color['RED']))
 # For example, the following return the RED member of the Color enumeration by its value:
 print(Color(1))
 print(Color(2))
+
+""" Iterate enumeration members """
+for color in Color:
+    print('color', color)
+    
+# Also, you can use the list() function to return a list of members from an enumeration:
+print(list(Color))
+
+""" Enumerations are immutable """
+# Enumerations are immutable. 
+# It means you cannot add or remove members once an enumeration is defined. 
+# And you also cannot change the member values.
+
+# Color['ORANGE'] = 4 # TypeError
+
+""" Python enumeration example """
+class ResponseStatus(Enum):
+    PENDING = 'pending'
+    FULFILLED = 'fulfilled'
+    REJECTED = 'rejected'
+    
+""" Summary """
+# An enumeration is a set of members that have associated unique constant values.
+# Create a new enumeration by defining a class that inherits from the Enum type of the enum module.
+# The members have the same types as the enumeration to which they belong.
+# Use the enumeration[member_name] to access a member by its name and enumeration(member_value) to 
+# access a member by its value.
+# Enumerations are iterable.
+# Enumeration members are hashable. It means we can use enums as the dict key.
+# Enumerable are immuable.
+# Cannot inherits from an enumeration unless it has no members"""
