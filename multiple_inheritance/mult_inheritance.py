e16e04a7301d6fc91b0e2276c4d92c2b3729cfb4
Asad Hussain <asad.h1998@yahoo.com>
2022-08-29 10:41:34 +0500

    started multiple inheritance

---
+++ multiple_inheritance/mult_inheritance.py
@@ -0,0 +1,21 @@
+""" Introduction to the Python Multiple inheritance """
+# When a class inherits from a single class, you have single inheritance. 
+# Python allows a class to inherit from multiple classes. 
+# If a class inherits from two or more classes, you’ll have multiple inheritance.
+
+class Car:
+    def go(self):
+        print('Going ...')
+
+class Flyable:
+    def fly(self):
+        print('Flying ...')
+
+class FlyingCar(Car, Flyable):
+    # Since the FlyingCar inherits from Car and Flyable classes, it reuses the methods from those classes. 
+    # It means you can call the go() and fly() methods on an instance of the FlyingCar class like this
+    pass
+
+fc = FlyingCar()
+fc.go()
+fc.fly()
